22f02ce6738aa6bba6d4c56cdd0a1387833ebfc8
redicaled <redicaled@gmail.com>
2024-07-25 05:40:00 +0900

    nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,7 +6,13 @@ export const Nav = (props:any) => {
     const [colorstyle, SetColorstyle] =  useState("hover:text-t0-bg from-t0-text to-t0-text");
     const [buttonstyle, Setbuttonstyle] =  useState("hover:text-t0-bg hover:bg-t0-text");
     const [fillsvg, Setfillsvg] =  useState("fill-t0-text group-hover:fill-t0-bg");
-
+    const themes = [
+        { bg: "#ede8e4", text: "#000000" },
+        { bg: "#040f49", text: "#E06E12" },
+        { bg: "#272727", text: "#D3FF25" },
+        { bg: "#226246", text: "#FDBCD2" },
+        { bg: "#4F4D27", text: "#E8FF26" },
+    ];
     useEffect(() => {
         if (props.num === 0) {
             SetColorstyle("hover:text-t0-bg from-t0-text to-t0-text")
@@ -36,34 +42,34 @@ export const Nav = (props:any) => {
     }, [props.num]);
 
     return (
-        <nav className=" font-extrabold fixed w-full grid grid-cols-5 text-center lg:h-[20px] h-[14px] lg:text-xl text-sm ">
-            <AnchorLink className=" group lg:h-[20px] h-[14px] ease-in-out" href="#about">
-                <div className={` hover:pr-[10vw] hover:pl-[1vw] bg-left-bottom bg-gradient-to-r ${colorstyle} bg-[length:0%] bg-no-repeat group-hover:bg-[length:100%] transition-all duration-500 ease-out`}>
+        <nav style={{ borderColor: themes[props.num]?.text, backgroundColor: themes[props.num]?.bg}} className=" border-b-2 font-extrabold fixed w-full grid grid-cols-5 text-center lg:h-[8vh] h-[8vh] lg:text-3xl text-xs sm:text-sm z-50 ">
+            <AnchorLink className=" group ease-in-out" href="#about">
+                <div className={` h-full py-[2.5vh] hover:pr-[10vw] hover:pl-[1vw] bg-left-bottom bg-gradient-to-r ${colorstyle} bg-[length:0%] bg-no-repeat group-hover:bg-[length:100%] transition-all duration-500 ease-out`}>
                     About
                 </div>
             </AnchorLink>
-            <AnchorLink className="group lg:h-[20px] h-[14px] ease-in-out" href="#projects">
-            <div className={` hover:pr-[10vw] hover:pl-[1vw] bg-left-bottom bg-gradient-to-r ${colorstyle} bg-[length:0%] bg-no-repeat group-hover:bg-[length:100%] transition-all duration-500 ease-out`}>
+            <AnchorLink className="group ease-in-out" href="#projects">
+            <div className={`h-full  py-[2.5vh] hover:pr-[10vw] hover:pl-[1vw] bg-left-bottom bg-gradient-to-r ${colorstyle} bg-[length:0%] bg-no-repeat group-hover:bg-[length:100%] transition-all duration-500 ease-out`}>
             Projects
                 </div>
             </AnchorLink>
 
             <div>         
-                <button onClick={()=>{props.SetThemenum((props.num+1)%5)}} className={` lg:h-[20px] h-[14px] group`}> 
-                    <div className={`${fillsvg} lg:w-[8.5vh] w-[5.2vh] mt-[0.15vh] ${buttonstyle} transition-color duration-500 `}>
+                <button onClick={()=>{props.SetThemenum((props.num+1)%5)}} className={` group`}> 
+                    <div className={`${fillsvg} lg:w-[8vh] sm:w-[8vh] w-[6vh] mt-[0.15vh] ${buttonstyle} transition-color duration-500 `}>
                     <PirateSVG  />
                     </div>
                 </button>
             </div>
 
-            <AnchorLink className="group lg:h-[20px] h-[14px] ease-in-out" href="#archive">
-            <div className={` hover:pr-[10vw] hover:pl-[1vw] bg-left-bottom bg-gradient-to-r ${colorstyle} bg-[length:0%] bg-no-repeat group-hover:bg-[length:100%] transition-all duration-500 ease-out`}>
+            <AnchorLink className="group ease-in-out" href="#archive">
+            <div className={`h-full  py-[2.5vh] hover:pr-[10vw] hover:pl-[1vw] bg-left-bottom bg-gradient-to-r ${colorstyle} bg-[length:0%] bg-no-repeat group-hover:bg-[length:100%] transition-all duration-500 ease-out`}>
             Archive
                 </div>
             </AnchorLink>
 
-            <AnchorLink className="group lg:h-[20px] h-[14px] ease-in-out" href="#BcKim">
-            <div className={` hover:pr-[10vw] hover:pl-[1vw] bg-left-bottom bg-gradient-to-r ${colorstyle} bg-[length:0%] bg-no-repeat group-hover:bg-[length:100%] transition-all duration-500 ease-out`}>
+            <AnchorLink className="group ease-in-out" href="#BcKim">
+            <div className={`h-full  py-[2.5vh] hover:pr-[10vw] hover:pl-[1vw] bg-left-bottom bg-gradient-to-r ${colorstyle} bg-[length:0%] bg-no-repeat group-hover:bg-[length:100%] transition-all duration-500 ease-out`}>
             BcKim
                 </div>
             </AnchorLink>
