52d933b8c5aa44c6fdd44e8f452d10ae1409c3d3
redicaled <redicaled@gmail.com>
2025-01-22 23:58:47 +0900

    remove all

--- src/app/api/animate_svg/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET(): Promise<NextResponse> {
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || '';
-    const svgFileUrl = `${baseUrl}/zzanga1.svg`;
-
-    return NextResponse.redirect(svgFileUrl);
-}
