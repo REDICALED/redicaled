5140d6f64b66d50be6225c68b267e2bbb585d0cf
redicaled <redicaled@gmail.com>
2025-01-22 23:30:08 +0900

    svg 불러오는 방식  수정, env 경로 추가

--- src/app/api/animate_svg/route.ts
+++ src/app/api/animate_svg/route.ts
@@ -1,17 +1,8 @@
 import { NextResponse } from 'next/server';
-import fs from 'fs';
-import path from 'path';
 
 export async function GET(): Promise<NextResponse> {
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || '';
+    const svgFileUrl = `${baseUrl}/zzanga1.svg`;
 
-    const filePath = path.resolve('public', 'zzanga1.svg');
-    const svgFileContent = fs.readFileSync(filePath, 'utf8');
-
-
-    // 캐시 방지 헤더 추가
-    return new NextResponse(svgFileContent, {
-        headers: {
-            'Content-Type': 'image/svg+xml',
-        },
-    });
+    return NextResponse.redirect(svgFileUrl);
 }
